292224cf2614560550d738a3e2c7842104a0ea35
Eden Yeung <basktballer@gmail.com>
2019-04-08 15:55:19 +0000

    Step 5 complete

---
+++ http-functions.js
@@ -0,0 +1,21 @@
+var https = require("https");
+
+module.exports = function getHTML(options, callback) {
+    /* Your code here */
+    var buffer = [];
+
+    https.get(options, function(response) {
+      response.setEncoding('utf8');
+  
+      response.on('data', function(data) {
+        buffer.push(data);
+      });
+      response.on('end', function(){
+        console.log("Response stream complete.");
+        for (var i = 0; i < buffer.length; i++) {
+          console.log(buffer[i]);
+        }
+      });
+    });
+    
+  };
